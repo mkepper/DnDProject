406eb6a66aa5098c05a1314a3beb49c9ceae27a1
mkepper <mkepperkepper@gmail.com>
2023-10-22 12:34:53 -0500

    Removing HTML file based JS script
    Removing the script from the PHP file and calling it from it's .js location

--- characterCreate.php
+++ characterCreate.php
@@ -33,23 +33,6 @@
             <label for="subClass">SubClass</label><br>
             <select id="subClass" class="form-select" aria-label="Default select example"
             ng-model="selectedSubOption" ng-options="subOption for subOption in subOptions"></select>
-            <!-- <script>
-            var app = angular.module('myApp', []);
-            app.controller('myCtrl', function($scope) {
-              $scope.options = ['Option 1', 'Option 2', 'Option 3'];
-              $scope.subOptions = [];
-
-              $scope.updateOptions = function() {
-                if ($scope.selectedOption == 'Option 1') {
-                  $scope.subOptions = ['Suboption 1A', 'Suboption 1B', 'Suboption 1C'];
-                } else if ($scope.selectedOption == 'Option 2') {
-                  $scope.subOptions = ['Suboption 2A', 'Suboption 2B', 'Suboption 2C'];
-                } else if ($scope.selectedOption == 'Option 3') {
-                  $scope.subOptions = ['Suboption 3A', 'Suboption 3B', 'Suboption 3C'];
-                }
-              };
-            });
-          </script> -->
           </div>
           <div class="form-group">
             <label for="level">Level</label>
